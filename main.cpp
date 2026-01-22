866e74215044ae6ffa3b954511ba8785db1c9004
Dumisani365 <dumimashinini@gmail.com>
2026-01-22 23:05:45 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lesson 21
+//
+//  Created by Dumisani Mashinini on 2026/01/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!gubuy\n";
+    return EXIT_SUCCESS;
+}
